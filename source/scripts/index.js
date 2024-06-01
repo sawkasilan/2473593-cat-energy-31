159863ae0a313f382494ef9946a191a063c23cd6
sawkasilan <158088651+sawkasilan@users.noreply.github.com>
2024-06-01 19:10:56 +0300

    Адаптация мелких элементов  (#11)
    * Добавляет модули scss
    
    * Базовая стилизация index
    
    * Базовая стилизация catalog
    
    * update
    
    * Добавление векторной графики
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Адаптация сетки header
    
    * update
    
    * update
    
    * update
    
    * mobile adaptive
    
    * адаптация advantages и functions
    
    * Адаптация tablet
    
    * hero desktop
    
    * адаптация index
    
    * catalog mobile
    
    * адаптивные сетки
    
    * background cat
    
    * update
    
    * декорации
    
    * update
    
    * up
    
    * update
    
    * update
    
    * update

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,17 @@
 /* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector ('.nav__toggle');
+const mainHeader = document.querySelector ('.main-header');
+
+mainHeader.classList.remove('main-header--nojs');
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('nav--close')) {
+    nav.classList.remove('nav--close');
+    nav.classList.add('nav--open');
+  } else {
+    nav.classList.add('nav--close');
+    nav.classList.remove('nav--open');
+  }
+});
