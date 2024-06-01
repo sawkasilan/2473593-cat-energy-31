85808b9cbcb1d6591fc447099955bb9cddb39a7a
sawkasilan <sawkasilan@gmail.com>
2024-06-01 14:05:02 +0300

    update

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector ('.nav__toggle');
+
+nav.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (nav.classList.contains('nav--close')) {
+    nav.classList.remove('nav--close');
+    nav.classList.add('nav--open');
+  } else {
+    nav.classList.add('nav--close');
+    nav.classList.remove('nav--open');
+  }
+});
